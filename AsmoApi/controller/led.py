68bd8a686b5e845c3f01243ccf20ac4b04e0ca75
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-14 23:46:48 +0100

    Making the project run on non-pi platforms again.

--- AsmoApi/controller/led.py
+++ AsmoApi/controller/led.py
@@ -1,17 +1,25 @@
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+    GPIOAvailable = true
+except:
+    GPIOAvailable = False
 
-GPIO.setmode(GPIO.BCM)
+if GPIOAvailable:
+    GPIO.setmode(GPIO.BCM)
 
-BLUE_PIN = 13;
-RED_PIN = 19;
-GREEN_PIN = 26;
+    BLUE_PIN = 13;
+    RED_PIN = 19;
+    GREEN_PIN = 26;
 
-GPIO.setup(BLUE_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(RED_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(GREEN_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(BLUE_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(RED_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(GREEN_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 def toogleAllOff():
-    GPIO.output(BLUE_PIN, GPIO.LOW)
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-    return "Hallo"
+    if GPIOAvailable:
+        GPIO.output(BLUE_PIN, GPIO.LOW)
+        GPIO.output(RED_PIN, GPIO.LOW)
+        GPIO.output(GREEN_PIN, GPIO.LOW)
+        return "Success"
+    else:
+        return "not running on a pi or RPi.GPIO library not installed"
