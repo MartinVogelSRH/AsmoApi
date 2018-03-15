3550a81741f99e38bfa381ed46a61aa6f6c4aafa
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-15 01:37:25 +0100

    Bugfixing motor control enabling i2c in install.sh

--- AsmoApi/Install.sh
+++ AsmoApi/Install.sh
@@ -1,1 +1,2 @@
 sudo pip install web.py
+sudo raspi-config nonint do_i2c 0
--- AsmoApi/controller/motor.py
+++ AsmoApi/controller/motor.py
@@ -9,11 +9,17 @@ if GPIOAvailable:
     PBR1 = PicoBorgRev.PicoBorgRev()
     PBR1.Init()
 
-def setMotors (motor1speed, motor2speed):
+def setMotors (motor1speed = 0, motor2speed = 0):
+    motor1speed = float(motor1speed)
+    motor2speed = float(motor2speed)
+    if abs(motor1speed) > 1:
+        motor1speed = motor1speed / float(100)
+    if abs(motor2speed) > 1:
+        motor2speed = motor2speed / float(100)
     if GPIOAvailable:
         PBR1.SetMotor1(motor1speed)
         PBR1.SetMotor2(motor2speed)
         return "Success"
     else:
-        print("not running on a pi. Motor 1 would have been set to: " + motor1speed + " Motor 2 would have been set to: " + motor2speed)
-        return "not running on a pi. Motor 1 would have been set to: " + motor1speed + " Motor 2 would have been set to: " + motor2speed
+        print("not running on a pi. Motor 1 would have been set to: " + str(motor1speed) + " Motor 2 would have been set to: " + str(motor2speed))
+        return "not running on a pi. Motor 1 would have been set to: " + str(motor1speed) + " Motor 2 would have been set to: " + str(motor2speed)
