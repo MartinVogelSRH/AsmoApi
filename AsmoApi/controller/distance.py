11deaa2f43b54153f813c7ab7bc2d13801334b28
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-15 23:52:25 +0100

    bugfix

--- AsmoApi/controller/distance.py
+++ AsmoApi/controller/distance.py
@@ -14,17 +14,17 @@ if GPIOAvailable:
 
 def getDistance():
     if GPIOAvailable:
-        GPIO.output(TRIG, False)                 #Set TRIG as LOW
+        GPIO.output(TRIG_PIN, False)                 #Set TRIG as LOW
         time.sleep(2)                            #Delay of 2 seconds
 
-        GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+        GPIO.output(TRIG_PIN, True)                  #Set TRIG as HIGH
         time.sleep(0.00001)                      #Delay of 0.00001 seconds
-        GPIO.output(TRIG, False)                 #Set TRIG as LOW
+        GPIO.output(TRIG_PIN, False)                 #Set TRIG as LOW
 
-        while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
+        while GPIO.input(ECHO_PIN)==0:               #Check whether the ECHO is LOW
             pulse_start = time.time()              #Saves the last known time of LOW pulse
 
-        while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+        while GPIO.input(ECHO_PIN)==1:               #Check whether the ECHO is HIGH
             pulse_end = time.time()                #Saves the last known time of HIGH pulse 
 
         pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
