0de05782c6bc8301ae248baa5346a8e991e64009
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-17 00:34:58 +0100

    camera

--- AsmoApi/routes/camera.py
+++ AsmoApi/routes/camera.py
@@ -1,6 +1,7 @@
 import web
 import time
 from io import BytesIO
+from PIL import Image
 try:
     import picamera
     GPIOAvailable = True
@@ -11,11 +12,17 @@ class Camera(object):
     """description of class"""
     def GET(self):
         if GPIOAvailable:
+            stream = BytesIO()
             cam = picamera.PiCamera()
             cam.resolution = (640, 480)
-            #web.header('Content-type','image/jpg')
+            web.header('Content-type','image/jpg')
             cam.start_preview()
             time.sleep(2)
-            cam.capture('../static/current.jpg')
-            web.seeother('../static/current.jpg')
+            camera.capture(stream, format='jpeg')
+            #cam.capture('/home/pi/Asmo/AsmoApi/AsmoApi/static/current.jpg')
+            cam.close()
+            stream.seek(0)
+            image = Image.open(stream)
+            return image
+            #web.seeother('../static/current.jpg')
 #app.route('/pic').post(pic.makePic);
