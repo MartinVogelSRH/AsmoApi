3f78397ec8165be4159a9a136d8909f0453d8f2a
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-17 01:28:40 +0100

    Camera

--- AsmoApi/routes/camera.py
+++ AsmoApi/routes/camera.py
@@ -19,13 +19,27 @@ class Camera(object):
             web.header('Transfer-Encoding','chunked') 
             cam.start_preview()
             time.sleep(2)
-            for i in range (0,60,1):
-                cam.capture(stream, format='jpeg')
-                #cam.capture('/home/pi/Asmo/AsmoApi/AsmoApi/static/current.jpg')
-                cam.close()
+            for foo in cam.capture_continuous(stream, 'jpeg', use_video_port=True):
                 stream.seek(0)
-                time.sleep(1)
-                #image = Image.open(stream)
-                yield stream
+                #time.sleep(1)
+                image = Image.open(stream)
+                yield image.toString()
                 #web.seeother('../static/current.jpg')
 #app.route('/pic').post(pic.makePic);
+
+    def getone():
+        if GPIOAvailable:
+            stream = BytesIO()
+            cam = picamera.PiCamera()
+            cam.resolution = (640, 480)
+            web.header('Content-type','image/jpg')
+            web.header('Transfer-Encoding','chunked') 
+            cam.start_preview()
+            time.sleep(2)
+            cam.capture(stream, format='jpeg')
+            #cam.capture('/home/pi/Asmo/AsmoApi/AsmoApi/static/current.jpg')
+            cam.close()
+            stream.seek(0)
+            #image = Image.open(stream)
+            return stream
+            #web.seeother('../static/current.jpg')
