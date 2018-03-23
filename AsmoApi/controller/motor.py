7b5530bf03e676bbb23b9a57672c27656a9506f3
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-24 00:34:32 +0100

    .

--- AsmoApi/controller/motor.py
+++ AsmoApi/controller/motor.py
@@ -1,4 +1,4 @@
-from controller.distance import DistanceController
+from controller.distance import DistanceController as DistCon
 try:
     import RPi.GPIO as GPIO
     import PicoBorgRev
@@ -21,7 +21,7 @@ def setMotors (motor1speed = 0, motor2speed = 0):
     if GPIOAvailable:
         PBR1.SetMotor1(motor1speed)
         PBR1.SetMotor2(motor2speed)
-        DistanceController().getDistance() #starting the read of distances. This will stop the motor if the distance is too low.
+        DistCon().getDistance() #starting the read of distances. This will stop the motor if the distance is too low.
         return "Success"
     else:
         print("not running on a pi. Motor 1 would have been set to: " + str(motor1speed) + " Motor 2 would have been set to: " + str(motor2speed))
