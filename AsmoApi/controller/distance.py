e88a25e2d1be3033f1139d713d703ec79bf01fa7
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-03-24 01:54:48 +0100

    .

--- AsmoApi/controller/distance.py
+++ AsmoApi/controller/distance.py
@@ -43,7 +43,7 @@ class DistanceController(object):
             while ((time.time() - cls.last_access < 10) or (motor1speed != 0) or (motor2speed != 0)):
                 print ('motor1: ' + str(motor1speed) + ' motor2: ' + str(motor2speed) + ' reding dist..')
                 GPIO.output(TRIG_PIN, False)                 #Set TRIG as LOW
-                time.sleep(2)                            #Delay of 2 seconds
+                time.sleep(2)                            #Delay of 2 seconds. This is a initialization timeframe. This should possibly be reduced.
 
                 GPIO.output(TRIG_PIN, True)                  #Set TRIG as HIGH
                 time.sleep(0.00001)                      #Delay of 0.00001 seconds
@@ -69,9 +69,9 @@ class DistanceController(object):
                 if abort:
                     cls.currentDist = 'No Sensor available or out of Range. Reading Aborted'
                 elif distance > 2 and distance < 400:      #Check whether the distance is within range
-                    if distance < MAX_DISTANCE:
-                        controller.motor.setMotors(0,0)
-                    cls.currentDist = str(distance - 0.5) + " cm"  #Print distance with 0.5 cm calibration
+                    if distance < MAX_DISTANCE and ((motor1speed + motor2speed) > 0):
+                        controller.motor.setMotors(0,0) #Stop the motors if the pi is moving forward.
+                    cls.currentDist = str(distance - 0.5) + " cm"  #return distance with 0.5 cm calibration
                 else:
                     cls.currentDist = "Out Of Range"                   #display out of range
             cls.thread = None
