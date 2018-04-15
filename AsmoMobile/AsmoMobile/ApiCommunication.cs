7d58e48426ca1f9f54e6c339b9429ae90fab4c29
MartinVogelSRH <Martin.Vogel@stud.hochschule-heidelberg.de>
2018-04-16 01:17:46 +0200

    Adding the baseline of a Xamarin App Interface

---
+++ AsmoMobile/AsmoMobile/ApiCommunication.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace AsmoMobile
+{
+    class ApiCommunication
+    {
+        public static void setMotorSpeed(int speed, int direction)
+        {
+            int motor1speed = 0; //left wheels
+            int motor2speed = 0; //right wheels
+            if (direction < 0)
+            {
+                //drive left
+                motor1speed = speed + (speed * (direction / 100)) * 2;
+                motor2speed = speed;
+            }
+            else if (direction > 0)
+            {
+                //drive right
+                motor1speed = speed;
+                motor2speed = speed + (speed * ((-1 * direction) / 100) * 2);
+            }
+            else if (direction == 0)
+            {
+                //drive straight
+                motor1speed = speed;
+                motor2speed = speed;
+            }
+            WebClient clientAsmo = new WebClient();
+            var reqparm = new System.Collections.Specialized.NameValueCollection();
+            reqparm.Add("motor1", motor1speed.ToString());
+            reqparm.Add("motor2", motor2speed.ToString());
+            clientAsmo.UploadValues("http://localhost:8080/api/motor", "POST", reqparm);
+
+
+        }
+    }
+}
---
+++ AsmoMobile/AsmoMobile/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AsmoMobile
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        void sliSpeedValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            lbl_speed.Text = args.NewValue.ToString();
+            ApiCommunication.setMotorSpeed((int)args.NewValue, (int)sliDirection.Value);
+        }
+        void sliDirectionValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            lbl_direction.Text = args.NewValue.ToString();
+            ApiCommunication.setMotorSpeed((int)sliSpeed.Value, (int)args.NewValue);
+        }
+    }
+}
